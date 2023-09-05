9c7499c096f749687cc7a81f427d2428a2e3cabc
Edwin <evdbelt@hotmail.com>
2023-09-05 16:32:21 +0200

    Import vehicles from file

---
+++ admin/clear_controller.py
@@ -0,0 +1,3 @@
+class ClearController():
+    def clear():
+        pass
--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
 import connexion
+import flask
+from flask import request
 
 from swagger_server import encoder
+from admin.import_controller import ImportController
+from admin.clear_controller import ClearController
 
+FUNCTIONS_ROUTES = [ {"url": "admin/import", "func": ImportController().import_all},
+                     {"url": "admin/clear", "func": ClearController().clear} 
+                    ]
+
+def create_view_func(func):
+    def view_func():
+        dict_output = func()
+        return flask.jsonify(dict_output)
+    return view_func
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'Dutch profile for data exchange between shared mobility providers and municipalities.'}, pythonic_params=True)
+    app.add_api('swagger.yaml', arguments={'title': 'Dutch profile CDS-M.'}, pythonic_params=True)
+    for func in FUNCTIONS_ROUTES:
+        app.add_url_rule(rule=f"/{func['url'] }",
+                        endpoint=func['func'].__name__,
+                        view_func=create_view_func(func['func']),
+                        methods=["GET"])
     app.run(port=8080)
-
 
 if __name__ == '__main__':
     main()
